545f55dd9064950b99f9b416826eb2369e38c140
Dylan Modell <dylan.modell.promo@gmail.com>
2019-01-05 20:36:56 -0800

    added a refresh feature to madlib

--- lib/src/screens/mad_lib.dart
+++ lib/src/screens/mad_lib.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:random_words/random_words.dart';
 
-class MadLib extends StatelessWidget{
-  final noun = WordNoun.random();
-  final adjective = WordAdjective.random();
+class MadLib extends StatefulWidget{
+  State<StatefulWidget> createState() => _MadLibState();
+}
+
+class _MadLibState extends State<MadLib>{
+  WordNoun noun = WordNoun.random();
+  WordAdjective adjective = WordAdjective.random();
 
   Widget build (BuildContext context){
     return Scaffold(
@@ -15,6 +19,17 @@ class MadLib extends StatelessWidget{
             'Mr. ${noun.asCapitalized} wrote a $adjective app in Flutter.'
           ),
         ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.refresh),
+          onPressed: _reloadMadLib,
+        ),
       );
+  }
+
+  void _reloadMadLib(){
+    setState(() {
+          noun = WordNoun.random();
+          adjective = WordAdjective.random();
+        });
   }
 }
